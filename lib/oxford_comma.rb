6890dbe70f9b251563903b1a030ed1183e4b86a2
4anth0ny1 <acatullo4@gmail.com>
2020-12-11 16:50:04 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+ 
+    if array.length == 1
+      return array.join()
+    elsif array.length == 2
+      return array.insert(-2,"and").join(' ')
+    elsif array.length > 2
+     three_array = array
+     three_array[-1] = "and #{three_array[-1]}"
+     three_array.join(', ')     
+    end
+ end
 
-end
+
+
