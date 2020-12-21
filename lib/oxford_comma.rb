e80c3b17bbe41e3cc2d6145f4ef0f385fba962bc
4anth0ny1 <acatullo4@gmail.com>
2020-12-20 18:52:07 -0600

    commit tree 6169cf985f2fe0ba2bb88e2c97671dc2e916c695

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     if array.length == 1
       return array.join()
     elsif array.length == 2
-      return array.insert(-2,"and").join(' ')
+      return "#{array.first} and #{array.last}" 
     elsif array.length > 2
      three_array = array
      three_array[-1] = "and #{three_array[-1]}"
